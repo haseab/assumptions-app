00c15d375377db6b17df5d7b7a8391217c11d09c
Haseab <hullah3000@gmail.com>
2023-11-06 01:30:38 -0800

    add criteria page

---
+++ app/criteria/page.tsx
@@ -0,0 +1,53 @@
+export default function Criteria() {
+  return (
+    <div className="max-w-3xl space-y-5 p-5 mt-5">
+      <h1 className="text-xl">Unacceptance Scale</h1>
+      <p>
+        <strong>1. Completely unbothered</strong> — This situation wouldn&apos;t
+        affect me at all.
+      </p>
+      <p>
+        <strong>2. Very mildly unacceptable</strong> — Not my first choice, but
+        would be able to manage without much inconvenience
+      </p>
+      <p>
+        <strong>3. Mildly Unacceptable</strong> — It would be a slight
+        resistance, but I would be able to adapt without major stress.
+      </p>
+      <p>
+        <strong>4. Bothered</strong> - Clearly not ideal, though not a major
+        problem.
+      </p>
+      <p>
+        <strong>5. Slightly unacceptable</strong>: It would start to challenge
+        my comfort levels. I would still cope, but I would wish things were
+        different.
+      </p>
+      <p>
+        <strong>6. Moderately Unacceptable</strong> — It has reached a point
+        where it&apos;s more than just uncomfortable; it&apos;s now a noticeable
+        disruption or intrusion in my life.
+      </p>
+      <p>
+        <strong>7. Unacceptable</strong> — It has crossed a line where I&apos;d
+        strongly desire an intervention or change. Tolerating it feels like a
+        compromise to my standards or values.
+      </p>
+      <p>
+        <strong>8. Highly Unacceptable</strong>: The situation is not only
+        against my preferences, but is challenging my core principles or
+        well-being. Proactive measures are strongly considered to address or
+        counteract its effects.
+      </p>
+      <p>
+        <strong>9. Extremely Unacceptable</strong>: Beyond just being not okay,
+        it verges on being intolerable; I&apos;d take serious measures to avoid
+        or rectify this situation before it escalates further.
+      </p>
+      <p>
+        <strong>10. Absolutely unacceptable</strong>: It would be impossible to
+        accept; It would be intolerable and immediate change would be needed.
+      </p>
+    </div>
+  );
+}
