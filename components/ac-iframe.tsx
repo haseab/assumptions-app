6a7be7cfe11008c96e9ab83b7822b6bb102ec151
Haseab <hullah3000@gmail.com>
2023-11-11 02:11:47 -0800

    remove share iframe, and add chat iframe

--- components/ac-iframe.tsx
+++ components/ac-iframe.tsx
@@ -1,23 +1,34 @@
-// pages/page.tsx
+"use client";
 
-import React from "react";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 
-const FullPageIframe: React.FC = () => {
+const FullPageIframe = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const params = useParams();
+  const id = params?.id;
+
+  const { closeChatButton: closeChatButtonString } = Object.fromEntries(
+    searchParams!.entries()
+  );
+
+  const closeChatButton = closeChatButtonString === "true";
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        overflow: "hidden",
-        margin: 0,
-        padding: 0,
-      }}
-    >
-      <iframe
-        src="https://automatic.chat/chats/clnnq22vx018bmc0yxcg48980/share"
-        style={{ height: "100%", width: "100%", border: "none" }}
-        title="Full Page Iframe"
-      />
+    <div className="flex h-screen w-full flex-col items-center justify-center rounded-lg bg-white p-8">
+      <div>
+        <iframe
+          src={`https://automatic.chat/chats/clnnq22vx018bmc0yxcg48980?closeChatButton=${closeChatButton}`}
+          width="400px"
+          height="600px"
+          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px",
+            borderRadius: "10px",
+            margin: "auto",
+          }}
+        ></iframe>
+      </div>
     </div>
   );
 };
