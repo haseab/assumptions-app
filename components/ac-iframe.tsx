7b4159b8c300c641a94c5bbf03c689e7e4e99831
Haseab <hullah3000@gmail.com>
2023-11-12 21:58:27 -0800

    fix keyboard issue on mobile

--- components/ac-iframe.tsx
+++ components/ac-iframe.tsx
@@ -1,36 +1,46 @@
 "use client";
 
-import { useParams, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const FullPageIframe = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const params = useParams();
-  const id = params?.id;
+  const [isMobile, setIsMobile] = useState(false);
 
-  const { closeChatButton: closeChatButtonString } = Object.fromEntries(
-    searchParams!.entries()
-  );
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768); // or whatever breakpoint you prefer
+    };
 
-  const closeChatButton = closeChatButtonString === "true";
+    // Check on mount
+    checkIfMobile();
+
+    // Add event listener
+    window.addEventListener("resize", checkIfMobile);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", checkIfMobile);
+  }, []);
 
   return (
-    <div className="flex flex-row h-screen w-screen items-center justify-center rounded-lg bg-white">
-      <div className="h-screen w-screen lg:h-[600px] lg:w-[500px]">
-        <iframe
-          src={`https://automatic.chat/chats/clnnq22vx018bmc0yxcg48980?closeChatButton=${closeChatButton}`}
-          // src={`http://localhost:3000/chats/clotv15pr002clu4ekrrwyn31`}
-          width="100%"
-          height="100%"
-          style={{
-            boxShadow:
-              "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px",
-            borderRadius: "10px",
-            margin: "auto",
-          }}
-        ></iframe>
+    <>
+      <div className="flex flex-row h-screen w-screen items-center justify-center rounded-lg bg-white">
+        <div className="max-h-full w-full h-full md:w-[400px] md:h-[600px]">
+          <iframe
+            src={`https://automatic.chat/chats/clnnq22vx018bmc0yxcg48980?`}
+            // src={`http://localhost:3000/chats/clotv15pr002clu4ekrrwyn31`}
+            width="100%"
+            height="100%"
+            style={{
+              width: isMobile ? "100dvw" : "100%",
+              height: isMobile ? "100dvh" : "100%",
+              boxShadow:
+                "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(150, 150, 150, 0.2) 0px 0px 0px 1px",
+              borderRadius: "10px",
+              margin: "auto",
+            }}
+          ></iframe>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
