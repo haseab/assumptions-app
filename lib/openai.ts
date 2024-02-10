80bf193a13455ce165684048425b0b362f4cecfb
Dillion Verma <hello@dillionverma.com>
2024-02-10 14:24:42 -0500

    🐛 fix: openai test

--- lib/openai.ts
+++ lib/openai.ts
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 
 console.log("API KEY", process.env.OPENAI_API_KEY);
 console.log("API KEY 2", process.env.OPEN_AI_API_KEY);
+console.log("ALL ENV", process.env);
 
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
