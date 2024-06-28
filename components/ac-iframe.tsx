9cfe73768aed28c4b9fe0fd1a279af18f783f1f4
Haseab <hullah3000@gmail.com>
2024-06-28 00:51:26 -0700

    make assumptions.app wider and higher

--- components/ac-iframe.tsx
+++ components/ac-iframe.tsx
@@ -24,7 +24,7 @@ const FullPageIframe = () => {
   return (
     <>
       <div className="flex flex-row h-screen w-screen items-center justify-center rounded-lg bg-white">
-        <div className="max-h-full w-full h-full md:w-[400px] md:h-[600px]">
+        <div className="max-h-full w-full h-full md:w-[500px] md:h-[700px]">
           <iframe
             src={env.NEXT_PUBLIC_CHATBOT_LINK}
             width="100%"
